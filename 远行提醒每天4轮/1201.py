5f0b13a916c6759054855bcc0b746e7d1969b208
jinjidangnian <jinjidangnian@outlook.com>
2026-05-29 17:40:22 +0800

    Add files via upload

---
+++ 远行提醒每天4轮/1201.py
@@ -0,0 +1,16 @@
+from get_merchant_props import get_all_props
+
+# 调用函数获取数据
+props = get_all_props()
+
+result_0801_text = "【远行商人】 第2轮 \n"
+for prop in props:
+    name = prop.get("name")
+    prop_start_ts = prop.get("start_time") # 这里拿到的是原始的时间戳毫秒数
+    
+    if int(prop_start_ts / 14400000) % 6 == 1:
+        line = f"{name}\n"
+        result_0801_text += line
+        print(result_0801_text)
+print(1)
+print(result_0801_text)
